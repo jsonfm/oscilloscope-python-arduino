987636c6947c87d7d54060e29ad837e5c595c357
Jason Francisco Macas Mora <franciscomacas3@gmail.com>
2022-05-25 15:48:10 -0500

    some timers added

--- oscilloscope/utils.py
+++ oscilloscope/utils.py
@@ -1,5 +1,85 @@
 import numpy as np
+import time
 
+
+class SamplerTimeCounter:
+    """Records the time interval between samples."""
+    def __init__(self):
+        self.lastTime = 0
+        self.currentTime = 0
+        self.elapsed = 0
+    
+    def interval(self):
+        """Calculates the elapsed time interval between the last measurement and the current one. (secs)."""
+        self.lastTime = self.currentTime
+        self.currentTime = time.time()
+        self.elapsed = self.currentTime - self.lastTime
+        return self.elapsed
+
+    def frequency(self):
+        """Calculates the frequency of the measurement time. (Hz)."""
+        try:
+            return 1 / self.interval()
+        except Exception as e:
+            return 0
+
+    def lastInterval(self):
+        """Returns the last measured time interval (secs)."""
+        return self.elapsed
+
+    def lastFrequency(self):
+        """Returns the last measured frequency. (Hz)."""
+        try:
+            return 1 / self.elapsed
+        except:
+            return 0
+
+    def update(self):
+        """Updates the time measurement."""
+        self.interval()
+
+    def clear(self):
+        """Clears internal variables."""
+        self.lastTime = 0
+        self.currentTime = 0
+
+
+class TimerCount:
+    """Counts elapsed time in seconds and checks if an interval of time had happen.
+    
+    Args:
+        interval: time interval in seconds
+        callback: it will be called when the time interval have been reached
+    
+    """
+    def __init__(self, interval: float = 1.0, callback = None):
+        self.interval = interval
+        self.callback = callback
+        self.time = 0
+        self.initial = 0
+        self.ready = False
+    
+    def update(self):
+        """Updates the time count."""
+        if self.initial == 0:
+            self.initial = time.time()
+        
+        # Interval have been reached
+        if time.time() - self.initial >= self.interval:
+            if self.callback is not None:
+                self.callback()
+            self.initial = 0
+            self.ready = True
+    
+    def isReady(self):
+        """Checks if the time interval was reached."""
+        return self.ready
+    
+    def reset(self):
+        """Resets the timer."""
+        self.initial = 0
+        self.ready = False
+        
 
 def magnitudeSpectrum(x, dt=1, Fs=1):
     N = len(x)
--- run.py
+++ run.py
@@ -1,36 +1,36 @@
-from socket import timeout
 import sys
-import signal
 import os
 from PyQt5.QtCore import QTimer
-from oscilloscope.base import OscilloscopeBase
-from oscilloscope.utils import dbScale
 from PyQt5.QtCore import pyqtSignal
 from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog
-import numpy as np
-from threading import Thread
-import json
-import time
+from oscilloscope.serialio import Serial
+from oscilloscope.buffer import Buffer
+from oscilloscope.utils import dbScale, SamplerTimeCounter, TimerCount
+
 
 pathUI = os.path.dirname(os.path.abspath(__file__))
 pathUI = os.path.join(pathUI, 'gui', 'main.ui')
 
 
 PEN_COLOR = (0, 190, 255)
-FPS = 10
+FPS = 15
 
 
-class Oscilloscope(QMainWindow, OscilloscopeBase):
+class Oscilloscope(QMainWindow):
+    """An oscilloscope app."""
     def __init__(self):
         super().__init__()
         uic.loadUi(pathUI, self)
         self.__configureGraph()
         self.__configureSerial()
-        # self.__configureBuffer()
+        self.__configureBuffer()
         self.__configureButtons()
-        self.t0 = 0
-        self.t1 = 0
+        self.__configureTimers()
+
+    def __configureBuffer(self):
+        """Configures the buffer."""
+        self.buffer = Buffer(maxlen=200)
 
     def __configureGraph(self):
         """Configures the signal graph."""
@@ -50,10 +50,19 @@ class Oscilloscope(QMainWindow, OscilloscopeBase):
         """Configures the serial device variables."""
         self.baudrates.addItems(['1200', '2400', '4800', '9600', '14400', '19200', '28800', '31250', '57600', '115200'])
         self.baudrates.setCurrentIndex(3)
-        self.updateListOfPorts(self.serial.ports())
+        self.serial = Serial(name="arduino", timeout=.5, emitAsDict=False)
         self.serial.on('connection', self.updateSerialConnectionStatus)
         self.serial.on('ports', self.updateListOfPorts)
         self.serial.on('data', self.updateBuffer)
+        self.updateListOfPorts(self.serial.ports())
+
+    def __configureTimers(self):
+        """Configures timers."""
+        self.samplerTimerCounter = SamplerTimeCounter()
+        self.frequencyLabelTimer = TimerCount(
+            interval=1, 
+            callback = lambda: self.fsLabel.setText(f"{self.samplerTimerCounter.lastFrequency():.5f}")
+        )
 
     def updateSerialConnectionStatus(self, status: bool):
         """Updates the connection status on the GUI."""
@@ -65,19 +74,20 @@ class Oscilloscope(QMainWindow, OscilloscopeBase):
         self.devices.addItems(ports)
 
     def updateBuffer(self, data: str):
-        self.t1 = time.time()
+        """Updates signal buffer values."""
         self.buffer.append(float(data))
-        if self.t1 != self.t0:
-            dt = self.t1 - self.t0
-            self.t0 = self.t1
-            print("dt: ", 1 // dt)
+        self.frequencyLabelTimer.update()
+        self.samplerTimerCounter.update()
 
-    def updateGraph(self):       
+    def updateGraph(self):   
+        """Plots the signal over the corresponding GUI element."""    
         data = self.buffer.getData()
         self.curve.setData(data)
+
+        # When the buffer is full clear it
         if self.buffer.isFull():
             self.buffer.clear()
-
+        
     def updatePortDevice(self):
         """Updates a new value for the port device."""
         device = self.devices.currentText()
@@ -87,12 +97,18 @@ class Oscilloscope(QMainWindow, OscilloscopeBase):
         else:
             self.serial.disconnect(force=True)
     
+    def start(self):
+        """Starts the serial read loop."""
+        self.serial.start()
+
     def close(self, event):
-        self.stop()
+        """Stops serial thread."""
+        self.serial.stop()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     w = Oscilloscope()
     w.show()
-    w.start()
+    w.start() # 
     sys.exit(app.exec_())
